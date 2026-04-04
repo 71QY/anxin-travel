3d850c2a7e254a7c55a85dc07d18adf63885448f
HY <13686961704@163.com>
2026-04-04 14:35:04 +0800

    fix: 订单创建接口兼容 poiName/poiLat/poiLng 字段名，修复目的地名称为空的问题

--- src/main/java/com/anxin/travel/module/order/dto/CreateOrderRequest.java
+++ src/main/java/com/anxin/travel/module/order/dto/CreateOrderRequest.java
@@ -1,10 +1,16 @@
 package com.anxin.travel.module.order.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class CreateOrderRequest {
+    @JsonProperty({"destName", "poiName"})  // 兼容两种字段名
     private String destName;
+    
+    @JsonProperty({"destLat", "poiLat"})  // 兼容两种字段名
     private Double destLat;
+    
+    @JsonProperty({"destLng", "poiLng"})  // 兼容两种字段名
     private Double destLng;
 }
